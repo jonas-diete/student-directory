e47ce184eeb16a359ccf228c41511cae617b04d7
Jonas Diete <jonas.diete@gmail.com>
2022-08-11 13:19:10 +0000

    improved menu

--- directory.rb
+++ directory.rb
@@ -1,30 +1,43 @@
+@students = []
+@cohorts = [:january, :november, :august]
+
 def interactive_menu
-  students = []
-  cohorts = [:january, :november, :august]
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students(cohorts)
-      when "2"
-        print_header
-        print(students, cohorts)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "Unknown command"
-      end
+    print_menu
+    process(gets.chomp)
   end
 end
 
-def input_students(cohorts)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if !@students.empty?
+    print_header
+    print_students_list
+    print_footer
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Unknown command"
+  end
+end
+
+def input_students
   puts "Please enter the details of the students"
   puts "To finish, just hit return twice"
-  students = []
   puts "Please enter the name of the first student"
   name = gets.strip
   while !name.empty? do
@@ -33,20 +46,19 @@ def input_students(cohorts)
     puts "Please enter their country of birth:"
     country = gets.chomp
     cohort = ""
-    until cohorts.include? cohort
+    until @cohorts.include? cohort
       puts "Please enter their cohort:"
       cohort = gets.chomp.to_sym
     end
-    students << {name: name, cohort: cohort, hobby: hobby, country: country}
-    if students.count == 1
+    @students << {name: name, cohort: cohort, hobby: hobby, country: country}
+    if @students.count == 1
       puts "Now we have 1 student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     puts "Please enter the name of the next student"
     name = gets.strip
   end
-  students
 end
 
 def print_header
@@ -54,19 +66,22 @@ def print_header
   puts "-------------"
 end 
 
-def print(students, cohorts)
-  cohorts.each { |cohort|
-    students.each_with_index { |student, index| 
+def print_students_list
+  @cohorts.each { |cohort|
+    @students.each_with_index { |student, index| 
       if cohort == student[:cohort]
         puts "#{index + 1}. #{student[:name]}, hobby: #{student[:hobby]}, country of birth: #{student[:country]} (#{student[:cohort]} cohort)".center(100) 
       end
     }
   }
-
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
+  else
+    puts "Overall, we have #{@students.count} great students"
+  end
 end
 
 interactive_menu
