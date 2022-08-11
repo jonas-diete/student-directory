a6f89e65c06338faae4935c839be3c4b5dc950c8
Jonas Diete <jonas.diete@gmail.com>
2022-08-11 12:54:22 +0000

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,12 +1,50 @@
-def input_students
-  puts "Please enter the names of the students"
+def interactive_menu
+  students = []
+  cohorts = [:january, :november, :august]
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students(cohorts)
+      when "2"
+        print_header
+        print(students, cohorts)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "Unknown command"
+      end
+  end
+end
+
+def input_students(cohorts)
+  puts "Please enter the details of the students"
   puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp
+  puts "Please enter the name of the first student"
+  name = gets.strip
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+    puts "Please enter their hobby:"
+    hobby = gets.chomp
+    puts "Please enter their country of birth:"
+    country = gets.chomp
+    cohort = ""
+    until cohorts.include? cohort
+      puts "Please enter their cohort:"
+      cohort = gets.chomp.to_sym
+    end
+    students << {name: name, cohort: cohort, hobby: hobby, country: country}
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    puts "Please enter the name of the next student"
+    name = gets.strip
   end
   students
 end
@@ -16,15 +54,19 @@ def print_header
   puts "-------------"
 end 
 
-def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+def print(students, cohorts)
+  cohorts.each { |cohort|
+    students.each_with_index { |student, index| 
+      if cohort == student[:cohort]
+        puts "#{index + 1}. #{student[:name]}, hobby: #{student[:hobby]}, country of birth: #{student[:country]} (#{student[:cohort]} cohort)".center(100) 
+      end
+    }
+  }
+
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
