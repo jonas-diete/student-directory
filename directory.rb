3235d1d57c897f11b819dc77f10fae615d32314c
Jonas Diete <jonas.diete@gmail.com>
2022-08-12 14:11:45 +0000

    final version without csv use

--- directory.rb
+++ directory.rb
@@ -4,15 +4,15 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list"
+  puts "4. Load the list"
   puts "9. Exit"
 end
 
@@ -25,7 +25,7 @@ def process(selection)
     when "3"
       save_students
     when "4"
-      load_students
+      ask_for_file
     when "9"
       exit
     else
@@ -42,48 +42,77 @@ def show_students
 end
 
 def save_students
-  file = File.open("students.csv", "w")
-  @students.each { |student|
-    student_data = [student[:name], student[:cohort], student[:hobby], student[:country]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  }
-  file.close
-  puts "List saved to students.csv"
+  puts "Enter the filename:"
+  filename = STDIN.gets.chomp
+  if File.exist?(filename)
+    File.open("students.csv", "w") do |file|
+      @students.each do |student|
+        student_data = [student[:name], student[:cohort], student[:hobby], student[:country]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+      end
+    end
+    puts "List saved to students.csv"
+  else
+    puts "File doesn't exist."
+  end
 end
   
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each { |line|
-    name, cohort, hobby, country = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country}
-  }
-  file.close
+def load_students(filename = "students.csv")
+  File.open(filename, "r") do |file|
+    file.readlines.each do|line|
+      name, cohort, hobby, country = line.chomp.split(',')
+      @students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country}
+    end
+  end
+  puts "Students loaded successfully."
+end
+
+def ask_for_file
+  puts "Please enter the filename:"
+  filename = STDIN.gets.chomp
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}."
+  else
+    puts "Sorry #{filename} doesn't exist."
+  end
+end
+
+def try_load_students
+  filename = ARGV.first
+  if filename.nil?
+    load_students 
+    return
+  end
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}."
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
+  end
 end
 
 def input_students
   puts "Please enter the details of the students"
   puts "To finish, just hit return twice"
   puts "Please enter the name of the first student"
-  name = gets.strip
+  name = STDIN.gets.strip
   while !name.empty? do
     puts "Please enter their hobby:"
-    hobby = gets.chomp
+    hobby = STDIN.gets.chomp
     puts "Please enter their country of birth:"
-    country = gets.chomp
+    country = STDIN.gets.chomp
     cohort = ""
     until @cohorts.include? cohort
       puts "Please enter their cohort:"
-      cohort = gets.chomp.to_sym
+      cohort = STDIN.gets.chomp.to_sym
     end
     @students << {name: name, cohort: cohort, hobby: hobby, country: country}
-    if @students.count == 1
-      puts "Now we have 1 student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
+    @students.count == 1 ? (puts "Now we have 1 student") : (puts "Now we have #{@students.count} students")
     puts "Please enter the name of the next student"
-    name = gets.strip
+    name = STDIN.gets.strip
   end
 end
 
@@ -93,21 +122,16 @@ def print_header
 end 
 
 def print_students_list
-  @cohorts.each { |cohort|
-    @students.each_with_index { |student, index| 
-      if cohort == student[:cohort]
-        puts "#{index + 1}. #{student[:name]}, hobby: #{student[:hobby]}, country of birth: #{student[:country]} (#{student[:cohort]} cohort)".center(100) 
-      end
-    }
+  @students.each_with_index { |student, index| 
+    puts "#{index + 1}. #{student[:name]}, hobby: #{student[:hobby]}, country of birth: #{student[:country]} (#{student[:cohort]} cohort)"
   }
 end
 
 def print_footer
-  if @students.count == 1
-    puts "Overall, we have #{@students.count} great student"
-  else
-    puts "Overall, we have #{@students.count} great students"
-  end
+  print "Overall, we have #{@students.count} great student"
+  @students.count == 1  ? (print "\n") : (puts "s")
+  puts " "
 end
 
+try_load_students
 interactive_menu
